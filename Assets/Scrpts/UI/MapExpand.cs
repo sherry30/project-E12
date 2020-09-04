323dc1418325b2b602059045b38b0d3a793719fe
AwkNinja <asadahsan420@gmail.com>
2020-09-04 09:57:29 +0500

    Added minimap extend functionality

---
+++ Assets/Scrpts/UI/MapExpand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapExpand : MonoBehaviour
+{
+    public RectTransform zoom;
+    public Button mapExpandButton;
+
+    void Awake()
+    {
+        mapExpandButton.onClick.AddListener(ExpandMap);
+    }
+
+    //Handle the onClick event
+    public void ExpandMap()
+    {
+        zoom.position = new Vector3(299f, 295f, 0f);
+        zoom.sizeDelta = new Vector2(1980, 1745);
+        Debug.Log("Expanding Map...");
+    }
+    
+}
--- Assets/Scrpts/UI/minimapCamCont.cs
+++ Assets/Scrpts/UI/minimapCamCont.cs
@@ -17,7 +17,7 @@ public class minimapCamCont : MonoBehaviour
         }
     }
 
-    public static float constantPos = -59f;
+    public static float constantPos = -57f;
     /*void Update()
     {
         
